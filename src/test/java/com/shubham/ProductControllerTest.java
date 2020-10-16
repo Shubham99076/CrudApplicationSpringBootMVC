783836795f2722bd2456978cc3fb05cb15dd8876
Shubham99076 <43137817+Shubham99076@users.noreply.github.com>
2020-10-16 18:47:30 +0530

    Add files via upload

---
+++ src/test/java/com/shubham/ProductControllerTest.java
@@ -0,0 +1,27 @@
+//package com.shubham;
+//
+//
+//import com.shubham.service.ProductService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.testng.annotations.BeforeTest;
+//
+//public class ProductControllerTest {
+//
+//    @Autowired
+//    ProductService productRepository;
+//
+//    @BeforeTest
+//    @Test
+//    public void testList() throws Exception {
+//
+//productRepository.getAllProduct();
+//        System.out.println("Test Completed" +productRepository.getAllProduct());
+//    }
+//
+//    @Test
+//    public void testShowProduct() throws Exception {
+//
+//    }
+//
+//}
